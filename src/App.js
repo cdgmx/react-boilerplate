8bf59fa2df4a0dace2ea1be5a29736cb3b20ff70
cdgmx <iamcdgmx@gmail.com>
2022-02-07 18:34:29 +0800

    Added Login Functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Navbar from "./components/Navbar";
+import UsersPage from "./pages/UsersPage";
+import { useSelector } from "react-redux";
 
 function App() {
+  const userLogin = useSelector((state) => state.userLogin);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const { userInfo } = userLogin;
+  useEffect(() => {
+    if (userInfo === null) {
+      setLoggedIn(false);
+    } else {
+      setLoggedIn(true);
+    }
+  }, [userInfo, setLoggedIn]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/users" element={<UsersPage />} />
+        </Routes>
+      </Router>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style.css';
+import store from './store';
+import  {Provider} from 'react-redux';
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
